328c8d1473dcad463a9ec6c5c9cab14c14f47dc2
devasatrio <34015427+devsatrio@users.noreply.github.com>
2020-08-11 19:24:14 +0700

    Added my project

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+Route::get('/home', 'backend\HomeController@index')->name('home');
+Route::get('/edit-profile', 'backend\HomeController@editprofile')->name('editprofile');
+Route::post('/edit-profile/{id}', 'backend\HomeController@aksieditprofile');
+
+// Route::get('/data-admin','backend\AdminController@listdata'); with datatable plugin
+Route::resource('/admin','backend\AdminController');
