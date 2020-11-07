a26e8503ee6dd0a9ec8d605fcf4cab6d8861fd79
deva <satriosuklun@gmail.com>
2020-11-07 18:49:08 +0700

    try edit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,7 +83,7 @@
                 <div class="title m-b-md">
                     New GMK Gudang
                 </div> 
-            </div> 
+            </div>  
         </div>
-    </body>
+    </body> 
 </html>
